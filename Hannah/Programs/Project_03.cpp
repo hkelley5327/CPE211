91490b198b143a734c3f5d3fb5c7678cf0172002
Hannah Kelley <100729525+hkelley5327@users.noreply.github.com>
2024-09-03 11:14:54 -0500

    Update Project_03.cpp

---
+++ Hannah/Programs/Project_03.cpp
@@ -0,0 +1,41 @@
+// ****************************************
+// Program Title: Sample header file
+// Project File: Cpp_header.cpp
+// Name: Ron Bowman  replace with your name
+// Course Section: CPE-211-01  Replace with your course section
+// Lab Section: 4  replace with your lab section
+// Due Date: 01/19/05  Edit as necessary
+// program description: Description of  Edit as necessary
+// What the program does.
+// ****************************************
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+// constant definitions
+const double PI = 3.14159265;
+const double G = 9.80665;
+
+// initial varibal edefinitions
+float timeToMaxHeight = 0;
+float timeToMaxDis = 0;
+float maxHeight = 0;
+float maxDis = 0;
+float maxHorRange = 0;
+float initialVel = 0;
+float startingAngleDeg = 0;
+float startingAngleRad = 0;
+
+// start of Program
+int main() {
+  cout << "******************************" << endl << "***** Projectile Motion" << endl << "******************************" << endl <<endl
+  cout << "Enter in the intial velocity (in meters/sec): "
+  cin >> 
+
+
+
+  return 0;
+}
