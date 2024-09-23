155f3aa5fbba54aeba58d52a29a904c5e0d4775a
Hannah Kelley <100729525+hkelley5327@users.noreply.github.com>
2024-09-23 14:35:00 -0500

    Project_05 draft
    very incomplete, much debugging needed

---
+++ Hannah/Programs/Project_05.cpp
@@ -0,0 +1,152 @@
+//include all necessary headers
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  //declare starting variables
+  double a, b, c;
+  string guess;
+  string type;
+  double area, perimeter;
+  string filename;
+  bool triangle;
+
+  //code from Mr. Bowman, testing iostream file input name given?
+  if(argc != 2) {
+    cout << endl;
+    cout << string(9,'*') << "Command Line Argument Error "
+    << string(9,'*') << endl;
+    cout << "==> Incorrect number of Command Line Arguments!\n";
+    cout << "==> Command for running the program is:\n";
+    cout << "==> ./Project_05 inputFileName\n";
+    cout << string(47,'*') << endl << endl;
+    return 1;
+  }
+
+  //header print
+  cout <<endl << string(60, '*') << endl;
+  cout << string(20, '*') << "  Triangle Checker  " << string(20, '*') << endl;
+  cout << string(60, '*') << "\n\n";
+
+  cout << "Opening Input File: " << filename;
+
+  //io stream starting conditions
+  ifstream inFile;
+  ofstream outFile;
+  string inFileName = argv[1];
+  inFile.open(inFileName);
+
+  //tell user which file is being read
+  cout << "Opening input file: " << inFileName << endl;
+
+  //io stream test, code from project files
+  if(inFile.fail() )
+  {
+    cout << string(15, '*') << " File Open Error "
+    << string(15, '8') << endl;
+    cout << "==> Input file failed to open properly!!\n";
+    cout << "==> Attempted to open file: " << filename << endl;
+    cout << "==> Terminating program!!!\n";
+    cout << string(47, '*') << "\n\n";
+    return 1;
+  }
+
+  if(inFile.good()) {
+    cout << "Reading values from input file...\n\n";
+  }
+
+  //reading side lenghts and guess from file
+  int count = 0;
+
+  while (count < 4) {
+    char label;
+
+    inFile >> label;
+    if (label == 'a') {
+      inFile >> a;
+    }
+    else if (label == 'b') {
+      inFile >> b;
+    }
+    else if (label == 'c') {
+      inFile >> c;
+    }
+    else {
+      inFile >> guess;
+    }
+
+    count++;
+  }
+
+  if(inFile.bad()) {
+    cout << left << setfill(' ') << string(15, '*') << " File Read Error " << string(15, '*') << endl;
+    cout << "==> Error reading data from input file.\n";
+    cout << "==> Terminating program now...\n";
+    cout << string(47, '*') << "\n\n";
+
+    return 1;
+  }
+
+  //checking for negative input values
+  if ((a < 0) || (b < 0) || (c < 0)) {
+    cout << left << setfill(' ') << string(15, '*') << " File Data Error " << string(15, '*') << endl;
+    cout << "==> Error: One or more side lengths are negative.\n";
+    cout << "==> Terminating program now...\n";
+    cout << string(47, '*') << "\n\n";
+
+    return 1;
+  }
+
+  //checking for a triangle
+  if ((a < b + c) || (b < a + c) || (c < a + b)) {
+    triangle = true;
+
+    //math using given inputs
+    perimeter = a + b + c;
+    double s = perimeter / 2;
+    area = sqrt(s * (s-a) * (s-b) * (s-c));
+
+    //checking type of triangle
+    if ((a==b) && (b==c)) {
+      type = "equilateral";
+    }
+    else if ((a==b) || (b==c) || (a==c)) {
+        type = "isosceles";
+    }
+    else {
+      type = "scalene";
+    }
+  }
+  else {
+    triangle = false;
+    type = "non-triangle";
+  }
+
+  //summary of data and results
+  cout << "Summary of Data Read...\n";
+  cout << fixed << setprecision(2) << "Side A = " << a << endl;
+  cout << fixed << setprecision(2) << "Side B = " << b << endl;
+  cout << fixed << setprecision(2) << "Side C = " << c << endl;
+  cout << "Guess  = " << guess << endl;
+  cout << string(60, '*');
+  cout << "Results From Data Read:";
+  cout << left << setfill('.') << setw(13) << "Type" << type << endl;
+  cout << left << setfill('.') << setw(13) << "Area" << fixed << setprecision(2) << area << endl;
+  cout << left << setfill('.') << setw(13) << "Perimeter" << fixed << setprecision(2) << perimeter << "\n\n";
+
+  if (guess == type) {
+    cout << "Congratulations!  Triangle specified is correct";
+  }
+  else {
+    cout << "Sorry! Triangle specidfied is incorrect" << endl;
+  }
+
+  cout << string(60, '*') << "\n\n";
+
+return 0;
+}
