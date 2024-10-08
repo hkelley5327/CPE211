2bf4462311977db857e22571bce9bd82c022fdbf
Hannah Kelley <100729525+hkelley5327@users.noreply.github.com>
2024-10-08 10:02:58 -0500

    Revert "g"
    This reverts commit c6ea9193f62e0ee64ee0b02ee6de07e65753de0a.

--- Hannah/Programs/Project_06.cpp
+++ Hannah/Programs/Project_06.cpp
@@ -137,8 +137,6 @@ int main(int argc, char *argv[]) {
   //while the end of the file has not been reached
   while (inFile.good()) {
 
-    getline(inFile, line, '\n');
-
     int i = 0;
 
     //reset line totals
@@ -193,7 +191,7 @@ int main(int argc, char *argv[]) {
     outFile << left << setw(15) << lineIndex << setw(10) << lineLetter << setw(10) << lineDigit << setw(10) << lineNeither <<  (lineLetter + lineDigit + lineNeither) << endl;
 
     //read in next line before looping back
-
+    getline(inFile, line, '\n');
   }
 
   //calculate percentages
