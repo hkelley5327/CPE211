0e42b87c86bf86fbed53b49c395bf7924b13d1b4
Hannah Kelley <100729525+hkelley5327@users.noreply.github.com>
2024-10-20 15:13:34 -0500

    project 8 final draft??

---
+++ Hannah/Programs/Project_08.cpp
@@ -0,0 +1,179 @@
+// ****************************************
+// Program Title: Project 7
+// Project File: Project_07.cpp
+// Name: Hannah Kelley
+// Course Section: CPE-211-02
+// Lab Section: 3
+// Due Date: 10/22/24
+// Program description: take a user input in a menu and either
+// approimates pi, flip a fair coin, roll a fair die, or exit the program
+// ****************************************
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+//structures
+struct Person {
+  string firstName;
+  string lastName;
+  string phoneNum;
+};
+
+struct Address {
+  string street;
+  string city;
+  string state;
+  int zip;
+};
+
+struct record{
+  Person person;
+  Address address;
+};
+
+//function prototypes
+void printHeader();
+void openInputFile();
+void openOutputFile();
+bool readInfo();
+void printInfo();
+
+int main() {
+  //io stream variable declarations
+  ifstream inputFile;
+  ofstream outputFile;
+
+  //open io streams
+  openInputFile(inputFile);
+  openOutputFile(outputFile);
+
+  //declare record structure
+  Record record;
+
+  printHeader(outputFile);
+
+  cout << "Processing information. Please wait....\n\n";
+
+  //while readInfo function returns true
+  while (readInfo(inputFile, record)) {
+    //print info for each entry
+    printInfo(record, outputFile);
+  }
+
+  //if we made it to the eof
+  if(inputFile.eof()) {
+    outputFile << setw(7) << string(5, '-') << setw(20) << string(9, '-') << setw(15) << string(10, '-') << setw(20) << string(4, '-') << string(12, '-') << endl;
+    cout << "Program has finished execution.\n\n";
+  }
+
+  return 0;
+}
+
+//function definitions
+void printHeader(ofstream& outputFile) {
+  outputFile << left << setw(7) << "Entry" << setw(20) << "Last Name" << setw(15) << "First Name" << setw(20) << "City" << "Phone Number\n";
+  outputFile << setw(7) << string(5, '-') << setw(20) << string(9, '-') << setw(15) << string(10, '-') << setw(20) << string(4, '-') << string(12, '-') << endl;
+}
+
+void openInputFile(ifstream& inputFile) {
+  string inputFileName;
+
+  //ask for and read in input file name
+  cout << "\nEnter the name of the input file: ";
+  cin >> inputFileName;
+  cout << inputFileName << "\n\n";
+
+  //open input file
+  inputFile.open(inputFileName.c_str());
+
+  //while in the fail state
+  while(inputFile.fail()) {
+    //error message
+    cout << string(15, '*') << " File Open Error " << string(15, '*') << endl;
+    cout << "==> Input file failed to open properly!!\n";
+    cout << "==> Attempted to open file: " << inputFileName << endl;
+    cout << "==> Please try again...\n";
+    cout << string(47, '*') << "\n\n";
+
+    //clear input file stream
+    inputFile.clear();
+
+    //ask for new file name
+    cout << "Enter the name of the input file: ";
+    cin >> inputFileName;
+    cout << inputFileName << "\n\n";
+
+    //attempt to open new file name
+    inputFile.open(inputFileName.c_str());
+  }
+}
+
+void openOutputFile(ofstream& outputFile) {
+  string outputFileName;
+
+  //ask for output file name
+  cout << "Enter the name of the output file: ";
+  cin >> outputFileName;
+  cout << outputFileName << "\n\n";
+
+  //attempt to open output file stream
+  outputFile.open(outputFileName.c_str());
+
+  //while output file stream in the fail state
+  while(outputFile.fail()) {
+    //error message
+    cout << string(15, '*') << " File Open Error " << string(15, '*') << endl;
+    cout << "==> Attempted to open file: " << outputFileName << endl;
+    cout << "==> Please try again...\n";
+    cout << string(47, '*') << "\n\n";
+
+    //clear output file stream
+    outputFile.clear();
+
+    //ask for different output file name
+    cout << "Enter the name of the output file: ";
+    cin >> outputFileName;
+    cout << outputFileName << "\n\n";
+
+    //attempt to open new file name
+    outputFile.open(outputFileName.c_str());
+  }
+}
+
+bool readInfo(ifstream& inputFile, Record& record) {
+  inputFile >> record.person.phoneNum;
+  inputFile >> record.person.lastName;
+  getline(inputFile, record.address.street, '\n');
+  getline(inputFile, record.address.city, '\n');
+  getline(inputFile, record.address.state, '\n');
+  inputFile >> record.address.zip;
+  inputFile >> record.person.firstName;
+
+  //after read, if in fail state
+  if(inputFile.fail()) {
+    //if not at the eof
+    if(!inputFile.eof()) {
+      //error message
+      cout << string(15, '*') << " File Read Error " << string(15, '*') << endl;
+      cout << "==> An error has occured while reading\n==> the input file. Error in file content.\n"
+      cout << "==> Terminating program!!!\n";
+      cout << string(47, '*') << "\n\n";
+    }
+
+    return false;
+  }
+
+  return true;
+}
+
+void printInfo(Record& record, ofstream& outputFile) {
+  //set entry number to zero, then increment each time the function is called
+  static int entry = 0;
+  entry++;
+
+  //print entry data
+  outputFile << left << setw(7) << entry << setw(20) << record.person.lastName << setw(15) << record.person.firstName << setw(20) << record.address.city << record.person.phoneNum << endl;
+}
